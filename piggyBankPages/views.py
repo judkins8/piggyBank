7b94a145c5c0592d756e968f4ae11ae218d9049b
amyzhang <amyrz22@byu.edu>
2023-11-05 20:53:54 -0700

    fixed

--- piggyBankPages/views.py
+++ piggyBankPages/views.py
@@ -1,4 +1,3 @@
-# views.py
 from django.shortcuts import render, redirect
 from .models import Transaction
 from .forms import TransactionForm, SavingsGoalForm
@@ -8,10 +7,10 @@ def index(request):
     if request.method == 'POST' and 'savings_goal' in request.POST:
         savings_goal_form = SavingsGoalForm(request.POST)
         if savings_goal_form.is_valid():
-            request.session['savings_goal'] = savings_goal_form.cleaned_data['savings_goal']
+            request.session['savings_goal'] = str(savings_goal_form.cleaned_data['savings_goal'])
             return redirect('index')
     else:
-        savings_goal_form = SavingsGoalForm(initial={'savings_goal': request.session.get('savings_goal', 0)})
+        savings_goal_form = SavingsGoalForm(initial={'savings_goal': request.session.get('savings_goal', '0')})
 
     # Handle transactions
     if request.method == 'POST' and 'amount' in request.POST:
@@ -23,19 +22,17 @@ def index(request):
         transaction_form = TransactionForm()
 
     transactions = Transaction.objects.all()
-    total_balance = sum(transaction.amount for transaction in transactions)
-    savings_goal = request.session.get('savings_goal', 0)
-
+    
     # Convert the Decimal to a string to preserve precision
     total_balance = sum(transaction.amount for transaction in transactions)
     total_balance = float(total_balance)  # Convert total_balance to float
-    savings_goal_str = "{:.2f}".format(savings_goal)
+    savings_goal = float(request.session.get('savings_goal', 0))
 
     context = {
         'transaction_form': transaction_form,
         'savings_goal_form': savings_goal_form,
         'transactions': transactions,
-        'total_balance': total_balance,  # Pass the string representation
-        'savings_goal': savings_goal_str,
+        'total_balance': total_balance,  # Now a float, not a Decimal
+        'savings_goal': savings_goal,
     }
     return render(request, 'piggyBankPages/index.html', context)
